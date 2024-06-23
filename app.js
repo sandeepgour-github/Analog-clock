2201e20ea5b9d2845253322eae6521f0a7f4287d
sandeepgour-git <sandeepgour597@gmail.com>
2024-06-23 11:04:17 +0530

    My Project

---
+++ app.js
@@ -0,0 +1,18 @@
+let hr = document.getElementById("hr");
+let min = document.getElementById("mn");
+let sec = document.getElementById("sc");
+function displayTime() {
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+
+  let hRotation = 30 * hh + mm / 2;
+  let mRotation = 6 * mm;
+  let sRotation = 6 * ss;
+
+  hr.style.transform = `rotate(${hRotation}deg)`;
+  min.style.transform = `rotate(${mRotation}deg)`;
+  sec.style.transform = `rotate(${sRotation}deg)`;
+}
+setInterval(displayTime, 1000);
